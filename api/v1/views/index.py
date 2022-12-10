0de7865d9df64bdb1fc162d56767248333c969c9
Ivanzkaa <ivanzka@me.com>
2022-12-10 12:35:27 -0400

    fixing the route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route("/status", strict_slashes=False)
 def status():
     """returning the status"""
     return jsonify({"status": "OK"}), 200
