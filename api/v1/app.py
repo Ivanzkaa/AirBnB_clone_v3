4a7c8dd035157ee49d9e787a058c97abc6a4f183
Ivanzkaa <ivanzka@me.com>
2022-12-10 21:55:01 -0400

    adding the error not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
 """the main flask app"""
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Blueprint, make_response
 from flask_cors import CORS
 from models import storage
 from os import getenv
 
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
-CORS(app, resources={r"/*": {"origins": '0.0.0.0'}})
-
-
 @app.teardown_appcontext
-def closing_sesh(self):
+def closing_sesh():
     """closing the session of the
     request"""
     storage.close()
 
 
+@app.errorhandler(404)
+def error_not_found():
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
 if (__name__ == "__main__"):
     host = getenv('HBNB_API_HOST')
     port = getenv('HBNB_API_PORT')
-
-    if not host:
-        host = '0.0.0.0'
-    elif not port:
-        port = '5000'
-
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
