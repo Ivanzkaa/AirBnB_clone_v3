f7f173acc9911452d37ae4b5a52884c9439b2457
Ivanzkaa <ivanzka@me.com>
2022-12-11 19:54:04 -0400

    seeing if this fixes the module documentation

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" starting a flask web app"""
-
-
-from flask import Blueprint
-from api.v1.views.states import *
+"""
+Start Flask Application
+"""
 from api.v1.views.cities import *
+from api.v1.views.states import *
+from flask import Blueprint
 
-
-app_views = Blueprint('app_views', __name__, url_prefix='/apiv1')
+app_views = Blueprint('app_views',  __name__, url_prefix='/api/v1')
