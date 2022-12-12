b543b975a6cf5af67a8cb0d8e7f1bce8ca5255c0
Ivanzkaa <ivanzka@me.com>
2022-12-11 22:09:43 -0400

    updating the innit file with the amenity

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-Start Flask Application
-"""
+"""Start Flask Application"""
 from api.v1.views.cities import *
 from api.v1.views.states import *
+from api.v1.views.amenities import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views',  __name__, url_prefix='/api/v1')
