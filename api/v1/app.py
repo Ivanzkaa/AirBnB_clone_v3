0df32a5a9b2fb00655f9926d88160cdc20588e7a
Ivanzkaa <ivanzka@me.com>
2022-12-11 20:50:10 -0400

    fixing th host and port

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,7 +24,7 @@ def error_not_found():
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
-if (__name__ == "__main__"):
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True, debug=True)
