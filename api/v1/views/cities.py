576019c818f675b6a66d2e1db0df2c0b7fc7ca00
Ivanzkaa <ivanzka@me.com>
2023-02-17 12:23:37 -0400

    fixing some errors

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """module for the city file"""
+
+
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models.city import City
-from models.state import State
 from models import storage
 
 
